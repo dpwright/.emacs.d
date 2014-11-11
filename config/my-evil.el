5a6ae7c72b5dd29868871becb12634887b14ad63
Daniel P. Wright <dani@dpwright.com>
2014-11-11 11:38:37 +0900

    Evil fixes

--- config/my-evil.el
+++ config/my-evil.el
@@ -16,10 +16,11 @@
   :ensure evil
   :init
     (progn
-      (setq evil-want-C-u-scroll t)
       (evil-mode 1))
   :config
     (progn
+      (setq evil-want-C-u-scroll t)
+      (setq evil-want-C-w-in-emacs-state t)
       (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
       (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
       (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
