3630c0feb983dd13aaedcf85db7d40ddb5426824
Daniel P. Wright <dani@dpwright.com>
2014-11-13 09:55:31 +0900

    Don't remap window navigation in evil (I'm happy enough with C-w + dir)

--- config/my-evil.el
+++ config/my-evil.el
@@ -21,10 +21,6 @@
     (progn
       (setq evil-want-C-u-scroll t)
       (setq evil-want-C-w-in-emacs-state t)
-      (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-      (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-      (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-      (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
       (evil-define-key 'normal emacs-lisp-mode-map (kbd "K")
         'elisp-slime-nav-describe-elisp-thing-at-point)))
 
