44dd942ce29bdb86409aed31bd7883d30fce59e4
Daniel P. Wright <dani@dpwright.com>
2014-12-12 19:38:40 +0900

    Fix evil keybindings in ibuffer

--- config/my-ibuffer.el
+++ config/my-ibuffer.el
@@ -1,11 +1,12 @@
 (eval-after-load 'ibuffer
   '(progn
-     (evil-set-initial-state 'ibuffer-mode 'normal)
-     (evil-define-key 'normal ibuffer-mode-map
-       (kbd "J") 'ibuffer-jump-to-buffer
-       (kbd "j") 'evil-next-line
-       (kbd "k") 'evil-previous-line
-       (kbd "l") 'ibuffer-visit-buffer
-       (kbd "C-]") 'ibuffer-visit-buffer)))
+     (after 'evil
+       (evil-set-initial-state 'ibuffer-mode 'normal)
+       (evil-define-key 'normal ibuffer-mode-map
+         (kbd "J") 'ibuffer-jump-to-buffer
+         (kbd "j") 'evil-next-line
+         (kbd "k") 'evil-previous-line
+         (kbd "l") 'ibuffer-visit-buffer
+         (kbd "C-]") 'ibuffer-visit-buffer))))
 
 (provide 'my-ibuffer)
