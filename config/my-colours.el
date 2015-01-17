2ddd765b7711f8d65be418cc6c4bcbf74cdb6d83
Daniel P. Wright <dani@dpwright.com>
2015-01-17 13:50:04 +0900

    Switch to a modified veersion of the 'minimal' theme

--- config/my-colours.el
+++ config/my-colours.el
@@ -1,7 +1,20 @@
-(use-package monokai-theme
-  :ensure monokai-theme
+(use-package minimal-theme
+  :ensure minimal-theme
   :init
-    (load-theme 'monokai t))
+    (progn
+      (load-theme 'minimal t)
+
+      (let ((comment-font (find-available-font
+                           '("CMU Concrete"
+                             "CMU Serif"
+                             "Georgia"))))
+        (dolist (face '(font-lock-comment-face
+                        font-lock-comment-delimiter-face))
+          (set-face-foreground face "gray60")
+          (set-face-italic     face t)
+          (set-face-font       face (concat comment-font " 14"))))
+
+      (set-face-background 'hl-line "MediumPurple4")))
 
 (use-package rainbow-blocks
   :ensure rainbow-blocks)
