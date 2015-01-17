ce68e85fe57ea38ed6d2f01a728277cf8c0057ba
Daniel P. Wright <dani@dpwright.com>
2015-01-17 13:52:26 +0900

    Set tab rules for elisp

--- config/pl/my-elisp.el
+++ config/pl/my-elisp.el
@@ -1,3 +1,7 @@
 (add-hook 'emacs-lisp-mode-hook 'rainbow-blocks-mode)
+(add-hook 'emacs-lisp-mode-hook
+          '(lambda()
+            (setq c-basic-offset 2)
+            (setq indent-tabs-mode nil)))
 
 (provide 'my-elisp)
