133becdacb0738c167e447cc18c6acd6291572c4
Daniel P. Wright <dani@dpwright.com>
2015-01-30 12:45:01 +0900

    Move fonts settings into my-fonts.el, and make it more emacs-y
    Set the variable-pitch and fixed-pitch defaults

--- config/my-colours.el
+++ config/my-colours.el
@@ -4,15 +4,11 @@
     (progn
       (load-theme 'minimal t)
 
-      (let ((comment-font (find-available-font
-                           '("CMU Serif"
-                             "CMU Concrete"
-                             "Georgia"))))
-        (dolist (face '(font-lock-comment-face
-                        font-lock-comment-delimiter-face))
-          (set-face-foreground face "gray60")
-          (set-face-italic     face nil)
-          (set-face-font       face (concat comment-font " 16"))))
+      (dolist (face '(font-lock-comment-face
+                      font-lock-comment-delimiter-face))
+        (set-face-foreground face "gray60")
+        (set-face-italic     face nil)
+        (set-face-font       face (font proportional-font 14)))
 
       (set-face-background 'hl-line "MediumPurple4")))
 
--- config/my-fonts.el
+++ config/my-fonts.el
@@ -5,18 +5,40 @@
         font
         (find-available-font (cdr fonts))))))
 
+(defun set-default-font-everywhere (font)
+  (set-face-attribute 'default nil :font  font )
+  (set-default-font font))
+
+(defvar proportional-font
+  (find-available-font '("CMU Serif"
+                         "CMU Concrete"
+                         "Georgia")))
+
+(defvar monospaced-font
+  (find-available-font '("CMU Typewriter Text"
+                         "Input"
+                         "Monaco"
+                         "Droid Sans Mono"
+                         "Courier New")))
+(defun font (name size)
+  (concat name " " (number-to-string size)))
+
 (defun lotr ()
   (interactive)
-  (set-default-font "Luminari 14"))
+  (set-default-font-everywhere (font "Luminari" 14)))
 
 (defun mono ()
   (interactive)
-  (set-default-font "CMU Typewriter Text 16"))
-
+  (set-default-font-everywhere (font monospaced-font 14)))
+variable-pitch
 (defun prop ()
   (interactive)
-  (set-default-font "CMU Concrete 16"))
+  (set-default-font-everywhere (font proportional-font 14)))
 
 (mono)
+
+(set-face-font 'variable-pitch (font proportional-font 14))
+(set-face-font 'fixed-pitch (font monospaced-font 14))
+(add-hook 'text-mode-hook 'variable-pitch-mode)
 
 (provide 'my-fonts)
