edcdff33ce4f0824846702f136d23d4e59e4cf09
Daniel P. Wright <dani@dpwright.com>
2015-02-12 11:30:31 +0900

    Switch to a dark-on-light colour scheme

--- config/my-colours.el
+++ config/my-colours.el
@@ -2,15 +2,14 @@
   :ensure minimal-theme
   :init
     (progn
-      (load-theme 'minimal t)
+      (load-theme 'minimal-light t)
 
       (dolist (face '(font-lock-comment-face
                       font-lock-comment-delimiter-face))
-        (set-face-foreground face "gray60")
         (set-face-italic     face nil)
         (set-face-font       face (font proportional-font 14)))
 
-      (set-face-background 'hl-line "MediumPurple4")))
+      (set-background-color "WhiteSmoke")))
 
 (use-package rainbow-blocks
   :ensure rainbow-blocks)
